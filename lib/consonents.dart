4c2c7ceb937ac0240c0425295b81663e6eaaeb8b
Asim Junaid <70271417+AsimJunaid1@users.noreply.github.com>
2020-09-17 23:21:01 +0530

    deleted consonents

--- lib/consonents.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-String kParseApplicationId= "AYomr4OU2YOxSbotWW53X8eZQnbYHWCiy5DDQKSK";
-String kParseClientKey = "e1HTQCWl7KqlAg0pfgdLiXEvS8jv2gLagWWBRgOZ";
-String kParseMasterKey = "37TCU8PQwdFVuZXkpYoy7IOhrlKDmbhI6aEIkCMU";
