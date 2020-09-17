39f0298cd7ad64d896b0a917238e53ab005c98ac
Asim Junaid <70271417+AsimJunaid1@users.noreply.github.com>
2020-09-17 21:53:37 +0530

    flutter-crud

--- lib/consonents.dart
+++ lib/consonents.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-String kParseApplicationId= "";
-String kParseClientKey = "";
-String kParseMasterKey = "";
-String kUrl= "";
+String kParseApplicationId= "AYomr4OU2YOxSbotWW53X8eZQnbYHWCiy5DDQKSK";
+String kParseClientKey = "e1HTQCWl7KqlAg0pfgdLiXEvS8jv2gLagWWBRgOZ";
+String kParseMasterKey = "37TCU8PQwdFVuZXkpYoy7IOhrlKDmbhI6aEIkCMU";
